029ab854a442e54a3f2d6b489113abf9aba57c35
riadesigner <evgenii.pogrebnyak@gmail.com>
2022-10-24 15:09:00 +1000

    ok

--- 003/js/site.js
+++ 003/js/site.js
@@ -8,16 +8,23 @@ var Maket = {
 		this.CANVAS_HEIGHT = this.$parent.height();
 		this.CANVAS_WIDTH = this.$parent.width();
 		this.PARAM = {
-			bgAlpha:true
+			bgAlpha:true,
+			ambient:0x777777,	
+			lightIntensity:.6,		
 		};
 
 		this.build_scene();	
+
 		this.build_light();
 		this.build_light_ambient();
 		
-		this.build_cube();
+		this.ARR_CUBES = [];	
+
+		this.build_cubes();
+
 		this.build_the_floor();
 		this.build_camera_helper();
+
 
 		this.animate();		
 	},
@@ -36,13 +43,26 @@ var Maket = {
 		this.camera.position.set(0,1,3);
 		this.camera.rotation.x += -0.4;
 	},
-	build_cube:function() {
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-		this.cube = new THREE.Mesh( geometry, material );
-		this.cube.castShadow = true; 
+	build_cubes:function() {
+		var arr = [1,1,1,1,1,1,1];
 
-		this.scene.add( this.cube );		
+		for(var i in arr){
+			var size =  Math.random()*.8;
+			var posX = Math.random()*6;
+			var posY = Math.random()*6;
+			var r = Math.floor(Math.random()*254);
+			var g = Math.floor(Math.random()*254);
+			var b = Math.floor(Math.random()*254);
+			var color = new THREE.Color("rgb("+r+", "+g+", "+b+")");
+			var geometry = new THREE.BoxGeometry( size, size, size );
+			var material = new THREE.MeshStandardMaterial( { color: color } );
+			var cube = new THREE.Mesh( geometry, material );
+			cube.position.set(posX-3,0,posY-3);
+			this.ARR_CUBES.push(cube);
+			cube.castShadow = true; 			
+			this.scene.add( cube );		
+		}
+		
 	},
 	build_the_floor:function() {
 		var geometry = new THREE.BoxGeometry( 3, .2, 3 );
@@ -54,7 +74,7 @@ var Maket = {
 		this.scene.add( this.the_floor );		
 	},	
 	build_light:function() {
-		this.light = new THREE.DirectionalLight( 0xffffff, .6 );
+		this.light = new THREE.DirectionalLight( 0xffffff, this.PARAM.lightIntensity );
 		this.light.position.set(0,4,0);
 		//add shadow param
 		this.light.castShadow = true;		
@@ -66,14 +86,21 @@ var Maket = {
 
 	},
 	build_light_ambient:function() {
-		this.amb_light = new THREE.AmbientLight( 0x777777 );
+		this.amb_light = new THREE.AmbientLight( this.PARAM.ambient );
 		this.scene.add( this.amb_light );				
 	},
 	animate:function() {
 		
 		var _this=this;
-		this.cube.rotation.y += 0.01;
-		this.cube.rotation.x += 0.01;		
+		for(var i in this.ARR_CUBES){			
+			// this.ARR_CUBES[i].position.x+=0.01;
+			// this.ARR_CUBES[i].position.z+=0.01;
+			this.ARR_CUBES[i].rotation.x+=0.01;
+			this.ARR_CUBES[i].rotation.z+=0.01;			
+		}
+		// this.cube.rotation.y += 0.01;
+		// this.cube.rotation.x += 0.01;	
+		// this.camera.rotation.z += -0.01;	
 		
 		this.renderer.render( this.scene, this.camera );		
 		window.requestAnimationFrame(function() {
